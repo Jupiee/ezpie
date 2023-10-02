372379b60aa1cc4e2caf25e28a19b01fca05afd8
Jupiee <mzkplayz1@gmail.com>
2023-10-02 21:00:05 +0500

    V0.3.0 refactored + creates requirements.txt now

--- src/main.rs
+++ src/main.rs
@@ -3,32 +3,39 @@ use std::fs;
 use std::env;
 use cli_prompts::{ prompts::{Input, AbortReason}, DisplayPrompt};
 
+struct File {
+
+    file_name: String,
+    content: String
+
+}
+
 fn dirname_validation(name: &str) -> Result<String, String> {
 
     // checks if the name has /, \ and : in it
     if check_dir(&name.to_owned()) {
 
-        return Err("Directory already exists".into())
+        return Err("Directory already exists".to_owned())
 
     }
 
     else if name.len() > 0 && !name.contains("/") && !name.contains("\\") && !name.contains(":") {
 
-        return Ok(name.to_string().replace(" ", "-"))  
+        return Ok(name.to_owned().replace(" ", "-"))  
 
     }
 
     else {
 
-        return Err("Invalid directory name".into())
+        return Err("Invalid directory name".to_owned())
 
     }
 
 }
 
 fn check_dir(dir_name: &String) -> bool {
-
-    let metadata= fs::metadata(format!("{}", dir_name));
+    
+    let metadata= fs::metadata(dir_name);
 
     match metadata {
 
@@ -39,30 +46,18 @@ fn check_dir(dir_name: &String) -> bool {
 
 }
 
-fn make_readme(dir_name: &String) {
+fn make_file(file: File) {
+    
+    let mut buffer_file= fs::File::create(file.file_name).unwrap();
 
-    let content= format!("## README documentation for {}", dir_name);
-
-    let mut file= fs::File::create("README.md").unwrap();
-
-    file.write_all(content.as_bytes()).unwrap();
-
-}
-
-fn make_gitignore() {
-
-    let content= format!(".DS_Store\n/src/__pycache__");
-
-    let mut file= fs::File::create(".gitignore").unwrap();
-
-    file.write_all(content.as_bytes()).unwrap()
+    buffer_file.write_all(file.content.as_bytes()).unwrap();
 
 }
 
 // creates a directory with python file
 fn make_src() {
     
-    fs::create_dir("./src".to_owned()).expect("Unable to create directory");
+    fs::create_dir("./src").expect("Unable to create directory");
 
     let main_content= "def main():\n\n\tprint('Hello World')\n\nif __name__ == '__main__':\n\n\tmain()";
 
@@ -89,10 +84,33 @@ fn main() {
 
     env::set_current_dir(format!("./{}", dir_name)).unwrap();
 
-    make_readme(&dir_name);
+    let readme= File{
+
+        file_name: "README.md".to_owned(),
+        content: "## README documentation".to_owned()
+
+    };
+
+    let gitignore= File {
+
+        file_name: ".gitignore".to_owned(),
+        content: "# Ignore files".to_owned()
+
+    };
+
+    let requirements= File {
+
+        file_name: "requirements.txt".to_owned(),
+        content: "## Requirements".to_owned()
+
+    };
+
+    make_file(readme);
+
+    make_file(gitignore);
+
+    make_file(requirements);
 
     make_src();
-
-    make_gitignore();
 
 }
